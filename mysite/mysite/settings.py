cdb88dbdcb0d80c47b7e58ed336fe44ac1f0e8f6
mywus <109961342+mywus@users.noreply.github.com>
2023-09-07 11:41:11 +0300

    added test for the questions model and mfixed question bug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "polls.apps.PollsConfig",
+    'rest_framework',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
